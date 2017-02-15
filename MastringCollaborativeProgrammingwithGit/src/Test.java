b47dde26a72ce140361cfc7a41913d85e15564d0
nabil <nberrehouma@gmail.com>
2017-02-15 18:26:57 +0100

    commit 1
    initial situation

---
+++ MastringCollaborativeProgrammingwithGit/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+ 
+	private String name ;
+	
+	public static void main (String args[])
+ {
+	 System.out.println("hello");
+	 
+	 System.out.println("hello2");
+	 System.out.println("hello");
+	 System.out.println("hello3");
+ }
+}
