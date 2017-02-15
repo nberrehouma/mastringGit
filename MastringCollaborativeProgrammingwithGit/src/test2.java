e6deee4e8dcb9555d363bc408779967cb7a26b34
nabil <nberrehouma@gmail.com>
2017-02-15 21:27:07 +0100

    ajout d'une boucle  à test2

--- MastringCollaborativeProgrammingwithGit/src/test2.java
+++ MastringCollaborativeProgrammingwithGit/src/test2.java
@@ -5,7 +5,13 @@ public class test2 {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("hello3");
+		int i=0;
+		while  (true){
+		i++;
+		 if(i==10 )break;
+		}
+		}
 
-	}
+	
 
 }
